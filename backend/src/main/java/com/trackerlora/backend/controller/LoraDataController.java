7389831f9d07ebcd84cfb4eae2813f0ac0162e1c
pcoloc <karatecafran@gmail.com>
2022-06-03 12:38:43 +0200

    cors

--- backend/src/main/java/com/trackerlora/backend/controller/LoraDataController.java
+++ backend/src/main/java/com/trackerlora/backend/controller/LoraDataController.java
@@ -7,6 +7,7 @@ import com.trackerlora.backend.service.LoraDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 // (2) Anotación de mapeo de petición en la URL /lora.
 @RequestMapping("/lora")
+@CrossOrigin(origins = "https://trackerlora.lopezcarrillo.com")
 public class LoraDataController {
 
     @Autowired
@@ -23,6 +25,7 @@ public class LoraDataController {
 
     // (4) Mapeo de petición GET a la URL /lora/data.
     @GetMapping("data")
+    @CrossOrigin(origins = "https://trackerlora.lopezcarrillo.com")
     public ResponseEntity<LoraData> getLoraData() {
         // (5) Devuelve una lista de objetos LoraData con los datos del scraping realizados por el servicio.
         return new ResponseEntity<LoraData>(loraDataService.retrieveLoraData(),
