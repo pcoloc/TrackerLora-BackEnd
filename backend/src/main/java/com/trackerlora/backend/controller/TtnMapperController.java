009020a7df33a58213c2fca8f5130abf5a2e3745
pcoloc <karatecafran@gmail.com>
2022-06-13 18:20:15 +0200

    no working

--- backend/src/main/java/com/trackerlora/backend/controller/TtnMapperController.java
+++ backend/src/main/java/com/trackerlora/backend/controller/TtnMapperController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
-@RequestMapping("/ttnmapper")
+@RequestMapping("/ttnMapper")
 @CrossOrigin(origins = {"https://trackerlora.lopezcarrillo.com", "http://localhost:4200"})
 public class TtnMapperController {
 
