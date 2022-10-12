fb4b42d115c50d355cf0246fb025a300c6673c66
pcoloc <karatecafran@gmail.com>
2022-10-12 17:27:41 +0200

    healthcheck without curl

--- backend/src/main/java/com/trackerlora/backend/BackendApplication.java
+++ backend/src/main/java/com/trackerlora/backend/BackendApplication.java
@@ -3,8 +3,6 @@ package com.trackerlora.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
