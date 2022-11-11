54cbeed61c45d8d7650e864ba81954a75e888a33
pcoloc <karatecafran@gmail.com>
2022-11-11 19:18:43 +0100

    added openapi

--- backend/src/main/java/com/trackerlora/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/trackerlora/backend/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
                 .group("springshop-public")
-                .pathsToMatch("/public/**")
+                .pathsToMatch("/**")
                 .build();
     }
 }
